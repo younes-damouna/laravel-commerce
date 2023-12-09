5e4ee1e100531b2b297800bcf8e64f16b0411040
younes_damouna <younes.damouna@gmail.com>
2023-12-10 01:47:20 +0200

    creating the migrations with seeding the users table

---
+++ app/Http/Controllers/UserDetailsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDetails;
+use Illuminate\Http\Request;
+
+class UserDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserDetails $userDetails)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserDetails $userDetails)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserDetails $userDetails)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserDetails $userDetails)
+    {
+        //
+    }
+}
